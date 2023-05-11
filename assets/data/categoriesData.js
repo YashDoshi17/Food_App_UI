fe099c599810a8782d42578f27222b2177a6a54e
Yash <yashdoshi1705@gmail.com>
2023-05-11 20:29:44 +0530

    commit

---
+++ assets/data/categoriesData.js
@@ -0,0 +1,22 @@
+const categoriesData = [
+    {
+        id:1,
+        image: require('../images/pizza-icon.png'),
+        title:'Pizza',
+        selcted:true
+    },
+    {
+        id:2,
+        image: require('../images/shrimp-icon.png'),
+        title:'Seafood',
+        selcted:false
+    },
+    {
+        id:3,
+        image: require('../images/soda-icon.png'),
+        title:'Soft Drinks',
+        selcted:false
+    },
+]
+
+export default categoriesData;
